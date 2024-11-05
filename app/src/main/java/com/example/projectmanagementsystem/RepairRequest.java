d9b181dd5f5544a320e906c0f38d26322ac09a63
Saloni260712 <patelsaloni.0711@gmail.com>
2024-11-05 01:10:39 -0500

    some changes in repair page

--- app/src/main/java/com/example/projectmanagementsystem/RepairRequest.java
+++ app/src/main/java/com/example/projectmanagementsystem/RepairRequest.java
@@ -15,12 +15,47 @@ public class RepairRequest extends AppCompatActivity {
 
         // Card view references
         CardView acRepairCard = findViewById(R.id.acRepairCard);
+        CardView mwRepairCard = findViewById(R.id.mwRepairCard);
+        CardView kitchenRepairCard = findViewById(R.id.kitchenRepairCard);
+        CardView plumbingCard = findViewById(R.id.plumbingCard);
+        CardView wmRepairCard = findViewById(R.id.wmRepairCard);
+        CardView pestControlCard = findViewById(R.id.pestControlCard);
 
         // Set click listeners on each card
         acRepairCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openRepairRequestPage("AC Repair"); // Pass the service name as needed
+            }
+        });
+        mwRepairCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRepairRequestPage("Microwave Repair"); // Pass the service name as needed
+            }
+        });
+        kitchenRepairCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRepairRequestPage("Kitchen Repair & Installation"); // Pass the service name as needed
+            }
+        });
+        plumbingCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRepairRequestPage("Plumbing"); // Pass the service name as needed
+            }
+        });
+        wmRepairCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRepairRequestPage("Washing Machine Repair"); // Pass the service name as needed
+            }
+        });
+        pestControlCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRepairRequestPage("Pest Control"); // Pass the service name as needed
             }
         });
     }
